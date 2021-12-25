1bb099c15ba6322e35fa0a592ac1dfa885e7154d
Carson Howard <cjhoward92@gmail.com>
2021-12-24 17:56:11 -0700

    Add more testing code

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main() {
     char *filename = "/tmp/test-db";
     int fd = open(filename, O_CREAT | O_RDWR, 0777);
     if (fd < 0) {
-        std:std::cerr << "Error: " << fd << std::endl;
+        std::cerr << "Error: " << fd << std::endl;
         return -1;
     }
 
@@ -29,19 +29,21 @@ int main() {
     std::cout << "File size: " << fsz << std::endl;
 
     BlockWriter writer(fd, 0);
+    BlockWriter writer2(fd, BLOCK_SIZE);
 
-    char buf[1024];
-    for (int i = 0; i < 1024; i++) {
-        buf[i] = 'a';
+    char buf[5000];
+    for (int i = 0; i < 5000; i++) {
+        buf[i] = (char)((i % 93) + 33);
     }
 
     writer.Write(buf, 1024);
+    writer2.Write(buf, 1024);
 
     fsz = file_len(filename);
     std::cout << "File size: " << fsz << std::endl;
 
     close(fd);
-    // unlink(filename);
+    unlink(filename);
 
     return 0;
 }
